dda990252c6061c688474834414478b58bff5666
andi103 <theandi103@gmail.com>
2014-02-23 17:25:21 +0100

    added a counterimage and some additions

---
+++ rockcounter.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class rockcounter here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class rockcounter extends Dolphin
+{
+    /**
+     * Act - do whatever the rockcounter wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
--- world.java
+++ world.java
@@ -17,7 +17,9 @@ public class world extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
+        addObject(new rockcounter(), 60, 150);
         addObject(new Dolphin(), 400, 300);
+        
     }
         public void started() { //Sets speed after pausing back to 50
         Greenfoot.setSpeed(50);
