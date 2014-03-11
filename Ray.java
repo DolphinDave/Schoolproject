df5f38bd49a9640c2458f06a9334468d00ce7a92
andi103 <theandi103@gmail.com>
2014-03-11 09:15:20 +0100

    changed some class structures

--- Ray.java
+++ Ray.java
@@ -8,12 +8,43 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Ray extends Animals
 {
+    private GreenfootImage ray = new GreenfootImage("Ray/ray.png");
+    private GreenfootImage ray1 = new GreenfootImage("Ray/ray1.png");
+    private int goupdown = 100;
+    private int steady = 0;
     /**
      * Act - do whatever the Ray wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        movearound();
     }    
+
+    public void movearound() {
+        int up = Greenfoot.getRandomNumber(50);
+        setLocation(getX() - 1, getY()); // just as fast as flow is
+        if (goupdown >= 35) {
+            setLocation(getX(), getY() + 1);
+            goupdown--;
+            setImage(ray);
+        }
+        else if (goupdown < 35 && goupdown >= 0 && up <= 5 && steady == 0) {
+            goupdown--;
+            steady++;
+            setImage(ray1);
+        }
+        else if (steady == 1 && goupdown >= 0) {
+            setLocation(getX(), getY() - 2);
+            goupdown--;
+        }
+        else if (steady == 1 && goupdown < 0) {
+            steady = 0;
+            goupdown = 100;
+        }
+        else if (goupdown < 0 && steady == 0) {
+            goupdown = 100;
+        }
+    }
+
 }
--- toxic.java
+++ toxic.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class toxic extends Dolphin
+public class toxic extends Animals
 {
     private int countanddelete = 70;
     /**
